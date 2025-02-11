9b1214feae0016619b51ca507953bb56b89e8f0f
Bhaskar <bhaskar.bid18@gmail.com>
2025-02-12 01:52:54 +0530

    Multi pdf upload code

--- MiniProject/MiniApp/models.py
+++ MiniProject/MiniApp/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    pic = models.FileField(upload_to='MiniApp_Images')
--- MiniProject/MiniApp/views.py
+++ MiniProject/MiniApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImagesForm
+from .models import Image
 
+# Create your views here.
+def index(request):
+    images = Image.objects.all()
+    context = {'images': images}
+    return render(request, "index.html", context)
+def fileupload(request):
+    form = ImagesForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        images = request.FILES.getlist('pic')
+        for image in images:
+            image_ins = Image(pic = image)
+            image_ins.save()
+        return redirect('index')
+    context = {'form': form}
+    return render(request, "upload.html", context)
 # Create your views here.
